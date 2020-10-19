09a66136ecba088051e226c57590db20578930b5
DeathKrow <irfanhgazi@gmail.com>
2020-10-19 10:45:17 +0530

    ``Subclasses added

--- src/buildAbox/BoxBuilder.kt
+++ src/buildAbox/BoxBuilder.kt
@@ -12,4 +12,7 @@ fun buildBox() {
     Box2.printSize()
     Box2.volume = 2000
     Box2.printSize()
+
+    val Cylinder = buildAcylinder(diameter = 25,height= 40)
+    Cylinder.printSize()
 }
--- src/buildAbox/buildAbox.kt
+++ src/buildAbox/buildAbox.kt
@@ -1,4 +1,5 @@
 package buildAbox
+import java.lang.Math.*
 
 open class buildAbox(open var length:Int = 10,open var width: Int = 10,open var height: Int = 10) {
     init{
@@ -9,7 +10,7 @@ open class buildAbox(open var length:Int = 10,open var width: Int = 10,open var 
         println("Width: $width cm " +
                 "Length: $length cm " +
                 "Height: $height cm ")
-        println("Volume: $volume m^3 Surface Area: $SurfaceArea")
+        println("Volume: $volume m^3 Surface Area: $SurfaceArea m^2")
     }
     constructor(volume: Int): this() {
         height = (volume/(length*width)).toInt()
@@ -23,4 +24,14 @@ open class buildAbox(open var length:Int = 10,open var width: Int = 10,open var 
 
     open var SurfaceArea: Double = 0.0
          get() = (length*width+width*height+length*height)*2.0
+}
+
+class buildAcylinder (override var height: Int, var diameter: Int): buildAbox(
+        height = height, width = diameter, length = diameter) {
+
+    override var volume: Int = 0
+        get() = (width / 2 * length / 2 * height * PI).toInt()
+
+    override var SurfaceArea: Double = (2*PI*length/2*height)+2*(PI*length/2*width/2)
+    override val shape = "Cylinder"
 }
