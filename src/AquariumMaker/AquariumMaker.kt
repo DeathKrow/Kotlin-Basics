19f8348e597c22c20e9a4d49e1a303b11a979f28
DeathKrow <irfanhgazi@gmail.com>
2020-11-06 15:34:20 +0530

    Edited Aquarium (Interface Delegation)

--- src/AquariumMaker/AquariumMaker.kt
+++ src/AquariumMaker/AquariumMaker.kt
@@ -4,9 +4,11 @@ fun makeAquarium() {
     val bowl = FishBowl()
     println(bowl.shape)
     bowl.clean()
+    println(bowl.color)
     val tank = FishTank()
     println(tank.shape)
     tank.clean()
+    println(tank.color)
 }
 fun main() {
     makeAquarium()
--- src/AquariumMaker/Aquaruim.kt
+++ src/AquariumMaker/Aquaruim.kt
@@ -7,14 +7,15 @@ abstract class Aquaruim {
     abstract val shape: String
 }
 
-class FishBowl: Aquaruim(), AquariumCleaner {
+class FishBowl: Aquaruim(), AquariumCleaner, AquariumColor {
     override val shape = "Sphere"
     override fun clean() {
         println("Cleaning fish bowl....")
     }
+    override val color = "Cyan"
 }
 
-class FishTank: Aquaruim(), AquariumCleaner {
+class FishTank: Aquaruim(), AquariumCleaner, AquariumColor by BlackColor {
     override val shape = "Cuboid"
     override fun clean() {
         println("Cleaning fish tank....")
@@ -23,4 +24,12 @@ class FishTank: Aquaruim(), AquariumCleaner {
 
 interface AquariumCleaner {
     fun clean()
+}
+
+interface AquariumColor {
+    val color: String
+}
+
+object BlackColor : AquariumColor{
+    override val color = "Black"
 }
